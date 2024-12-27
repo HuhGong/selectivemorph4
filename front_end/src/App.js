cd797c7fd3991e247ce4f721577cef6f57988403
kim <juntop223@gmail.com>
2024-12-27 13:45:30 +0900

    Home :  상단바랑 간격 조정 Transfer : 상단바랑 간격조정 , 제공되는이미지 미리 보이게 수정 -2

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -5,16 +5,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PhotoSelector from "./PhotoSelector";
 import Scroll from "./Scroll";
 import AnnotationViewer from './AnnotationViewer'; // 추가된 컴포넌트 임포트
+import Cover from './Cover';
 
 function App() {
     return (
         <Router>
             <div>
-                <Header />
+                <Cover />
+                {/*-<Header />*/}
                 <div className="main-container">
                     <Routes>
-                        <Route path="/" element={<Scroll />} />
-                        <Route path="/transfer" element={<PhotoSelector />} />
+                        <Route path="/" element={<Cover><Scroll /></Cover>} />
+                        <Route path="/transfer" element={<Cover><PhotoSelector /></Cover>} />
                         {/* 다른 경로에 대한 Route 추가 가능 */}
                     </Routes>
                 </div>
